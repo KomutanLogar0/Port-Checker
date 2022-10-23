4ad7bcd2cf23440ca36d685ef983dc011be9ed84
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2022-10-23 16:25:40 +0300

    Create portchecker.py

---
+++ portchecker.py
@@ -0,0 +1,31 @@
+import socket
+from pyfiglet import Figlet
+
+f = Figlet(font="standard")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print(f.renderText("Komutan Logar"))
+print(f.renderText("Port Checker / Finder"))
+checkorfind = int(input("Port Checker: 1, Port Finder: 2 "))
+
+if checkorfind == 1:
+    while True:
+     ip = input("What is your enemy ip? ")
+     port = input("What is your enemy port? ")
+     try:
+        s.connect((ip, port))
+        print("(✓) Port is active " + port)
+     except:
+        print("(✘) Port is deactive " + port)
+elif checkorfind == 2:
+    while True:
+        ip = input("What is your enemy ip?")
+        for port in range(1, 9999 + 1):
+            try:
+                s.connect((ip, port))
+                print("(✓) Port is active " + str(port))
+            except:
+                print("(✘) Port is deactive " + str(port))
+                
+
+

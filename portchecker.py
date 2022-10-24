49310b2895a659e4bb20c9699df75e49f4265898
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2022-10-24 17:36:24 +0300

    Update portchecker.py

--- portchecker.py
+++ portchecker.py
@@ -1,7 +1,7 @@
 import socket
 from pyfiglet import Figlet
 
-f = Figlet(font="standard")
+f = Figlet(font="slant")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print(f.renderText("Komutan Logar"))

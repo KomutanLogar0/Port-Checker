2ee26c31d1999682b31d97a0f51c96448419e16d
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2022-10-26 19:14:23 +0300

    Update portchecker.py

--- portchecker.py
+++ portchecker.py
@@ -8,26 +8,27 @@ print(f.renderText("Komutan Logar"))
 print(f.renderText("Port Checker / Finder"))
 checkorfind = int(input("Port Checker: 1, Port Finder: 2 "))
 
+
 if checkorfind == 1:
     while True:
-     ip = input("What is your enemy ip? ")
-     port = input("What is your enemy port? ")
+     ip = input("Düşman IP'yi giriniz.")
+     port = input("Düşman Port'u giriniz.")
      try:
         s.connect((ip, port))
-        print("(YES) Port is active " + port)
+        print("(E) Port aktif " + port)
      except:
-        print("(NO ) Port is deactive " + port)
+        print("(H) Port deaktif " + port)
 elif checkorfind == 2:
     while True:
-        ip = input("What is your enemy ip?")
+        ip = input("Düşman IP'yi giriniz.")
         for port in range(1, 9999 + 1):
             try:
                 s.connect((ip, port))
-                print("(YES) Port is active " + str(port))
+                print("(Y) Port aktif" + str(port))
                 f = open("openport.txt", "x")
                 f.write(port)
             except:
-                print("(NO) Port is deactive " + str(port))
+                print("(H) Port deaktif " + str(port))
                 
 
 

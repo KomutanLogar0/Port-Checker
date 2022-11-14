6a7b2faf69641d45bf86fc28c2c417ba49b4d616
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2022-11-14 13:22:12 +0300

    Update pfinder.py

--- pfinder.py
+++ pfinder.py
@@ -2,30 +2,30 @@ import socket
 from pyfiglet import Figlet
 
 f = Figlet(font="standard")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print(f.renderText("Komutan Logar"))
 print(f.renderText("Pfinder"))
 print("''Güneş tepeden vuruyosa gölge ayağımızın altıdır''")
-checkorfind = int(input("Port Checker: 1, Port Finder: 2 "))
+checkyadafind = int(input("Port Checker: 1, Port Finder: 2 "))
 
 
 
-if checkorfind == 1:
+if checkyadafind == 1:
     while True:
      ip = input("Düşman IP? ")
      port = input("Düşman Port? ")
      try:
-        s.connect((ip, port))
+        soket.connect((ip, port))
         print("(E) Port aktif " + port)
      except:
         print("(H) Port deaktif " + port)
-elif checkorfind == 2:
+elif checkyadafind == 2:
     while True:
         ip = input("Düşman IP?")
         for port in range(1, 9999 + 1):
             try:
-                s.connect((ip, port))
+                soket.connect((ip, port))
                 print("(E) Port aktif " + str(port))
                 f = open("openport.txt", "x")
                 f.write(port)

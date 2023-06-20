193e55fe7fae10e511abfeee6189144a64a4a14e
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2023-06-20 17:21:15 +0300

    Update pfinder.py

--- pfinder.py
+++ pfinder.py
@@ -1,33 +1,34 @@
 import socket
 from pyfiglet import Figlet
 
-f = Figlet(font="standard")
-soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+f = Figlet(font="slant")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print(f.renderText("Komutan Logar"))
-print(f.renderText("Pfinder"))
-print("''Güneş tepeden vuruyosa gölge ayağımızın altıdır''")
-checkyadafind = int(input("Port Checker: 1, Port Finder: 2 "))
+print(f.renderText("Port Checker / Finder"))
+checkorfind = int(input("Port Checker: 1, Port Finder: 2 "))
 
 
-
-if checkyadafind == 1:
+if checkorfind == 1:
     while True:
-     ip = input("Düşman IP? ")
-     port = input("Düşman Port? ")
+     ip = input("Karşı IP'yi giriniz.")
+     port = input("Karşı Port'u giriniz.")
      try:
-        soket.connect((ip, port))
+        s.connect((ip, port))
         print("(E) Port aktif " + port)
      except:
         print("(H) Port deaktif " + port)
-elif checkyadafind == 2:
+elif checkorfind == 2:
     while True:
-        ip = input("Düşman IP?")
+        ip = input("Karşı IP'yi giriniz.")
         for port in range(1, 9999 + 1):
             try:
-                soket.connect((ip, port))
-                print("(E) Port aktif " + str(port))
-                f = open("acikport.txt", "x")
+                s.connect((ip, port))
+                print("(Y) Port aktif" + str(port))
+                f = open("openport.txt", "x")
                 f.write(port)
             except:
                 print("(H) Port deaktif " + str(port))
+                
+
+
